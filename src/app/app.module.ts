c216bfbc9884a8a0616ba5889960f9292d400b2a
GuillermoDLCO <guillermo.dlco@outlook.com>
2019-09-14 23:06:35 -0500

    Correccion service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { ServiceCartService } from './service-cart.service';
 import { CartService } from './cart.service';
 
 @NgModule({
@@ -28,7 +27,7 @@ import { CartService } from './cart.service';
     ProductDetailsComponent
   ],
   bootstrap: [ AppComponent ],
-  providers: [ServiceCartService, CartService]
+  providers: [CartService]
 })
 export class AppModule { }
 
